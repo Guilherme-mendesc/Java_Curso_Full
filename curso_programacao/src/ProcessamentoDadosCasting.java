ea01ca39f734ab3828de91e711e7bc5912c3d755
Guilherme-mendesc <guimendes1337@gmail.com>
2022-05-18 17:53:32 -0300

    first commit ate aula30

---
+++ curso_programacao/src/ProcessamentoDadosCasting.java
@@ -0,0 +1,24 @@
+
+public class ProcessamentoDadosCasting {
+
+	public static void main(String[] args) {
+		
+		int a, b;
+		double resultado;
+		double ab;
+		int ba;
+		
+		a =5;
+		b =2;
+		ab=5.0;
+		ba=(int) a;
+		
+		resultado = (double) a/b;
+		System.out.println(ba);
+		System.out.println(resultado);
+
+	}
+
+
+
+}
