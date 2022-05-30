a889566fe69813c6c65e89022bd77c82297d75ce
Guilherme-mendesc <guimendes1337@gmail.com>
2022-05-30 17:19:33 -0300

    segunda lista exercicios

---
+++ curso_programacao/src/ExerciciosModCintoAoOito/ExercicioUm.java
@@ -0,0 +1,34 @@
+package ExerciciosModCintoAoOito;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ExercicioUm {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner (System.in);
+		
+		
+		double nota1=sc.nextDouble();
+		double nota2=sc.nextDouble();
+		
+		double notaFinal= nota1+ nota2;
+		System.out.printf("NOTA FINAL IGUAL = %.1f%n",notaFinal);
+		
+		if(notaFinal <60) {
+			System.out.printf("REPROVADO");
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+	}
+
+}
