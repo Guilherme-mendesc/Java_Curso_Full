728dcbd608646e95deb23cb5613cfd248f240c88
Guilherme Mendes Carvalho <gmendeca@emeal.nttdata.com>
2022-09-02 17:42:01 -0300

    atualizacao lista full

--- Modulo10/src/application/ExercicioFixacao11.java
+++ Modulo10/src/application/ExercicioFixacao11.java
@@ -24,13 +24,12 @@ public class ExercicioFixacao11 {
 			alturas[i] = sc.nextDouble();
 			System.out.printf("Genero da %da pessoa:  ", i+1);
 			generos[i] = sc.next().charAt(0);
-			
 		}
 		
 		menorAltura=alturas[0];
 		maiorAltura=alturas[0];
 		
-		for (int i = 0; i<n ; i++) {
+		for (int i =1; i<n ; i++) {
 			if(alturas[i]>maiorAltura) {
 				maiorAltura=alturas[i];
 			}
@@ -38,13 +37,29 @@ public class ExercicioFixacao11 {
 				menorAltura=alturas[i];
 			}
 		}
+		
 		quantidadeHomem=0;
 		quantidadeMulher=0;
 		alturaFemTotal=0;
 		
 		for(int i=0; i<n; i++) {
+			if(generos[i]=='M') {
+				quantidadeHomem++;
+			}
+			else {
+				quantidadeMulher++;
+				alturaFemTotal=alturaFemTotal+alturas[i];
+			}
 			
 		}
+		alturaFemMedia = alturaFemTotal/quantidadeMulher;
+		
+		System.out.printf("Menor altura = %.2f\n", menorAltura);
+		System.out.printf("Menor altura = %.2f\n", maiorAltura);
+		System.out.printf("Media das alturas das mulheres = %.2f\n", alturaFemMedia);
+		System.out.printf("Numero de homens = %d\n", quantidadeHomem);
+		
+		
 		
 		
 		sc.close();
