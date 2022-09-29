b974d3c2ef98e9047db414d0f062a207b55f453e
Guilherme Mendes Carvalho <gmendeca@emeal.nttdata.com>
2022-09-29 09:55:27 -0300

    polimorfmismo

---
+++ Modulo14/src/entitiesEmployee/OutSorcedEmployee.java
@@ -0,0 +1,34 @@
+package entitiesEmployee;
+
+public class OutSorcedEmployee extends Employee {
+	
+	
+	private Double additionalCharge;
+	
+	public  OutSorcedEmployee() {
+		
+	}
+	
+	public OutSorcedEmployee(String name, Integer hours, Double valuePerHour, Double additionalCharge) {
+		super(name, hours, valuePerHour);
+		this.additionalCharge = additionalCharge;
+	}
+
+	public Double getAdditionalCharge() {
+		return additionalCharge;
+	}
+
+	public void setAdditionalCharge(Double additionalCharge) {
+		this.additionalCharge = additionalCharge;
+	}
+	
+	
+	@Override
+	public double payment() {
+		return  super.payment()+ additionalCharge*1.1;
+	}
+	
+	
+	
+
+}
